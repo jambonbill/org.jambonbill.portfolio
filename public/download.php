8b1b264adcf2a9620b2df6f21963e2cb9234241f
jambonbill <jambonbill@gmail.com>
2023-03-20 13:38:19 +0100

    force download

--- public/download.php
+++ public/download.php
@@ -1,3 +1,13 @@
 <?php
 //dowload resume + Log
 
+$file_url = '/docs/cv-pierre-boquet.pdf';
+
+if (!is_file($file_url)) {
+	exit("file not found");
+}
+
+header('Content-Type: application/octet-stream');
+header("Content-Transfer-Encoding: Binary"); 
+header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\""); 
+readfile($file_url); 
