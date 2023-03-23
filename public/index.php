8338c1a5486aaf7cb4ee9a270ec0f43ff29d083a
jambonbill <jambonbill@gmail.com>
2023-03-23 12:05:03 +0100

    +$Parsedown = new \Parsedown();

--- public/index.php
+++ public/index.php
@@ -4,12 +4,40 @@ session_start();
 
 require __DIR__."/../vendor/autoload.php";
 
-
 $PF=new JAM\PortfolioItems();
+$Parsedown = new \Parsedown();
 
+$PF->fetch();//get api data
 
+$twigloader = new Twig\Loader\FilesystemLoader('./templates');
+$twig = new Twig\Environment($twigloader);
 
-echo '<pre>';print_r($_GET);echo '</pre>';
+if (empty($_GET['id'])) {
+    
+    // main
+    $items=$PF->list();
+    $template=$twig->load('main.twig');
+    echo $template->render([
+        'items'=>$items
+    ]);
 
-echo '<pre>';print_r($PF->fetch());echo '</pre>';
+} else {
+    
+    // card detail
+    $item=$PF->find($_GET['id']);//Project data
+    $item->description=$item->description_short;
+    
+    if ($item->description_long) {
+        $item->description=$Parsedown($item->description_long);
+    }
+    
 
+    $template=$twig->load('project.twig');
+    
+    echo $template->render([
+        'GET'=>print_r($_GET,true),
+        'item'=>$item,
+        'debug'=>print_r($item,true)
+    ]);    
+
+}
