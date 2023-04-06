3a5ba61194165400ab655d8fecd35eb28c40ecfd
jambonbill <jambonbill@gmail.com>
2023-04-06 12:51:13 +0200

    jambonbill unsorted skills

---
+++ public/skills.php
@@ -0,0 +1,37 @@
+<?php
+//jambonbill unsorted skills
+$skills=[];
+$skills[]='JavaScript/ES6';
+$skills[]='PHP7 & 8';
+//$skills[]='ES6';
+$skills[]='NodeJS';
+$skills[]='Python';
+$skills[]='C#';
+$skills[]='Composer';
+$skills[]='Packagist'; 
+$skills[]='Symfony';
+$skills[]='Npm';
+$skills[]='React';
+$skills[]='JQuery'; 
+$skills[]='D3.js';
+$skills[]='API REST';
+$skills[]='SOAP';
+$skills[]='Google API’s';
+$skills[]='AWS';
+$skills[]='MariaDB';
+$skills[]='MySQL';
+$skills[]='SQLite'; 
+$skills[]='PostGres';
+$skills[]='MongoDB'; 
+$skills[]='Redis';
+$skills[]='GIT';
+$skills[]='Docker';
+$skills[]='Composer';
+//$skills[]='Postfix';
+//$skills[]='Vagrant';
+$skills[]='Regexp';
+$skills[]='CRUD';
+$skills[]='PSR2';
+
+//sort($skills);
+
