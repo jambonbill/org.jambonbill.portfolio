f76d8f836833f543453c3852876d0d5f04184f00
jambonbill <jambonbill@gmail.com>
2023-04-14 07:46:33 +0200

    @error_log($row, 3, "../logs/download.log");

--- public/download.php
+++ public/download.php
@@ -1,6 +1,15 @@
 <?php
 //dowload resume + Log
 
+//logger
+//echo '<pre>';print_r($_SERVER);exit;
+$row=date('c')."\t";
+$row.=$_SERVER['REMOTE_ADDR']."\t";
+$row.=$_SERVER['HTTP_USER_AGENT']."\t";
+$row.=$_SERVER['REQUEST_URI']."\t";
+$row.=PHP_EOL;
+
+@error_log($row, 3, "../logs/download.log");
 
 $file_url = './cv/CV-Pierre-BOQUET.court.pdf';
 
